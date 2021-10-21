c8a5c35e3eaa1c18c7c4eb5932c579e1256b7766
yash gupta <yash3144@gmail.com>
2021-10-22 00:07:59 +0530

    Space

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App(){
   //   await this.loadWeb3()
   //   await this.loadBlockchainData()
   // }
-
+ 
   async function loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
